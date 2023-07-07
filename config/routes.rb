6ba9d64d21fd84e277f196a3d1ed8183860d2f6f
sato0w0 <130539468+sato0w0@users.noreply.github.com>
2023-07-07 13:08:48 +0000

    [Add]新規登録/ログイン/ログアウト機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+  # 顧客用
+  # URL /users/sign_in ...
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+  namespace :admin do
+    get '/' => 'homes#top'
+  end
+
+  scope module: :public do
+    root :to => 'homes#top'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
